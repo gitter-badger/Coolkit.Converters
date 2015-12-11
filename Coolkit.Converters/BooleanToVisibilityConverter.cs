eed936e375c0f7bc29604f669d39903325b06e57
Thijs Brobbel <t.brobbel@coolblue.nl>
2015-12-11 18:02:24 +0100

    Add first implementation of BooleanToVisibilityConverter.

--- Coolkit.Converters/BooleanToVisibilityConverter.cs
+++ Coolkit.Converters/BooleanToVisibilityConverter.cs
@@ -1,19 +1,30 @@
 ﻿using System;
 using System.Globalization;
+using System.Windows;
 using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace Coolkit.Converters
 {
-    public class BooleanToVisibilityConverter : IValueConverter
+    public class BooleanToVisibilityConverter : MarkupExtension, IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            if(!(value is bool))
+                return DependencyProperty.UnsetValue;
+
+            var castedValue = (bool)value;
+            return castedValue ? Visibility.Visible : Visibility.Collapsed;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return DependencyProperty.UnsetValue;
+        }
+
+        public override object ProvideValue(IServiceProvider serviceProvider)
+        {
+            return this;
         }
     }
 }
